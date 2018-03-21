15d3614ea5d06ec1b5c327e7db0925d880a19836
Allen Short <ashort@mozilla.com>
2018-03-21 20:38:48 +0000

    Properly rollback failed db commits

--- redash/handlers/data_sources.py
+++ redash/handlers/data_sources.py
@@ -56,6 +56,7 @@ class DataSourceResource(BaseResource):
         try:
             models.db.session.commit()
         except IntegrityError as e:
+            models.db.session.rollback()
             if req['name'] in e.message:
                 abort(400, message="Data source with the name {} already exists.".format(req['name']))
 
@@ -134,6 +135,7 @@ class DataSourceListResource(BaseResource):
 
             models.db.session.commit()
         except IntegrityError as e:
+            models.db.session.rollback()
             if req['name'] in e.message:
                 abort(400, message="Data source with the name {} already exists.".format(req['name']))
 
