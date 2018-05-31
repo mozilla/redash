be1acaf8438a80bc56d2d49bad6e2a6404d9069c
Allen Short <ashort@mozilla.com>
2018-05-31 10:47:48 -0500

    parameters widgets

--- client/app/components/DateTimeInput.jsx
+++ client/app/components/DateTimeInput.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import { react2angular } from 'react2angular';
 import DatePicker from 'antd/lib/date-picker';
 
-function DateTimeInput({
+export default function DateTimeInput({
   value,
   withSeconds,
   onSelect,
@@ -46,7 +46,4 @@ DateTimeInput.defaultProps = {
   onSelect: () => {},
 };
 
-export default function init(ngModule) {
-  ngModule.component('dateTimeInput', react2angular(DateTimeInput, null, ['clientConfig']));
-}
 
