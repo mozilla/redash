5bdd91e38c8832985d75cc7e1344f917afd355bc
Allen Short <ashort@mozilla.com>
2018-07-09 08:29:59 -0500

    schema browser

--- client/app/components/queries/schema-browser.js
+++ client/app/components/queries/schema-browser.js
@@ -1,43 +1,6 @@
-import template from './schema-browser.html';
-
-function SchemaBrowserCtrl($rootScope, $scope) {
-  'ngInject';
-
-  this.showTable = (table) => {
-    table.collapsed = !table.collapsed;
-    $scope.$broadcast('vsRepeatTrigger');
-  };
-
-  this.getSize = (table) => {
-    let size = 22;
-
-    if (!table.collapsed) {
-      size += 18 * table.columns.length;
-    }
-
-    return size;
-  };
-
-  this.isEmpty = function isEmpty() {
-    return this.schema === undefined || this.schema.length === 0;
-  };
-
-  this.itemSelected = ($event, hierarchy) => {
-    $rootScope.$broadcast('query-editor.command', 'paste', hierarchy.join('.'));
-    $event.preventDefault();
-    $event.stopPropagation();
-  };
-}
-
-const SchemaBrowser = {
-  bindings: {
-    schema: '<',
-    onRefresh: '&',
-  },
-  controller: SchemaBrowserCtrl,
-  template,
-};
+import { react2angular } from 'react2angular';
+import SchemaBrowser from '@/react-components/SchemaBrowser';
 
 export default function init(ngModule) {
-  ngModule.component('schemaBrowser', SchemaBrowser);
+  ngModule.component('schemaBrowser', react2angular(SchemaBrowser));
 }
